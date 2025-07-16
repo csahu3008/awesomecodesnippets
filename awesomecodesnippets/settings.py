0951dc18ce848290890c092599d99eeee0491e01
chandra2607 <107454567+chandra2607@users.noreply.github.com>
2025-07-16 07:43:13 +0530

    update db

--- awesomecodesnippets/settings.py
+++ awesomecodesnippets/settings.py
@@ -17,8 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import dj_database_url
 
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -132,6 +130,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
