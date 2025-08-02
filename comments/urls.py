45258712ef8e9c55062d50126e4053e2e506a1ef
chandra2607 <107454567+chandra2607@users.noreply.github.com>
2025-08-02 06:40:31 +0530

    fix: update regex

--- comments/urls.py
+++ comments/urls.py
@@ -4,7 +4,6 @@ urlpatterns=[
   path('add_comment/',AddComment.as_view(),name='add_comment') ,  
   path('add_reply/',AddReply.as_view(),name='add_reply') ,  
   path('child_comment/',GetChildComments.as_view(),name='child_comment')  ,
-  re_path('^tags/(?P<tag>[\w+]+)/',GetTaggedItems.as_view(),name='tag'),
-    path('rate/',HelpfulOrNot.as_view(),name='rating') ,  
- 
+  re_path(r'^tags/(?P<tag>[\w+]+)/',GetTaggedItems.as_view(),name='tag'),
+  path('rate/',HelpfulOrNot.as_view(),name='rating') ,  
 ] 
