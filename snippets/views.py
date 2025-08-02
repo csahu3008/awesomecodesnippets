8eec777b2520ef62b4c582f6fd71092ae7ce2bca
chandra2607 <107454567+chandra2607@users.noreply.github.com>
2025-08-02 08:16:54 +0530

    temp

--- snippets/views.py
+++ snippets/views.py
@@ -38,7 +38,7 @@ class HomePage(TemplateView):
         
         languages_and_counts={}
         print(LANGUAGE_CHOICES,"LANGUAGE_CHOICES")
-        print(LANGUAGE_CHOICES[0].__dict__(),"LANGUAGE_CHOICES[0].__dict__")
+        print(LANGUAGE_CHOICES[0].__dict__,"LANGUAGE_CHOICES[0].__dict__")
         try:
             for lang,lang_name in LANGUAGE_CHOICES:
                 count=Snippet.objects.filter(language=lang).count()
