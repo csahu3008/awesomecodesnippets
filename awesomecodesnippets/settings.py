b80af4a884f6d28bce7fe197112fe3fca398e919
csahu3008 <csahu3932@gmail.com>
2025-10-10 21:57:19 +0530

    disable iframe based middleware till data updated

--- awesomecodesnippets/settings.py
+++ awesomecodesnippets/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'allauth.account.middleware.AccountMiddleware', 
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = [
